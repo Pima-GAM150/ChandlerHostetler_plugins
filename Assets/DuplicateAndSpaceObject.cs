26f8ce3099833bfe45f73e1ef07f4372b2542f9e
Chandler <chosteler@mail.pima.edu>
2019-04-06 14:45:42 -0700

    added script for the object

---
+++ Assets/DuplicateAndSpaceObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicateAndSpaceObject : MonoBehaviour
+{
+    public int numberOfDuplications;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void duplicate() {
+
+    }
+}
