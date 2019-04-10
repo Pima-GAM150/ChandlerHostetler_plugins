5527c9348bb374610210bf68c0feb675abbe1f80
Chandler <chosteler@mail.pima.edu>
2019-04-09 23:40:04 -0700

    added a spacing modifier so you can change how much space between the objects

--- Assets/DuplicateAndSpaceObject.cs
+++ Assets/DuplicateAndSpaceObject.cs
@@ -7,6 +7,7 @@ public class DuplicateAndSpaceObject : MonoBehaviour
     public int numberOfDuplications;
     public List<GameObject> clones;
     public Transform mainObject;
+    public int spacing;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,7 +40,10 @@ public class DuplicateAndSpaceObject : MonoBehaviour
 
         foreach (GameObject clone in clones) {
             for (int i = 0;  i< numberOfDuplications; i++)
-            clones[i].transform.position = mainObject.TransformDirection(new Vector2(mainObject.position.x, mainObject.position.y - 1 - i));
+             if(i == 0)
+                clones[i].transform.position = mainObject.TransformDirection(new Vector2(mainObject.position.x, mainObject.position.y - spacing));
+            else
+                clones[i].transform.position = mainObject.TransformDirection(new Vector2(mainObject.position.x, mainObject.position.y - ((i+1) * spacing)));
         }
     }
 
@@ -49,7 +53,12 @@ public class DuplicateAndSpaceObject : MonoBehaviour
         foreach (GameObject clone in clones)
         {
             for (int i = 0; i < numberOfDuplications; i++)
-                clones[i].transform.position = mainObject.TransformDirection(new Vector2(mainObject.position.x + 1 + i, mainObject.position.y));
+                if (i == 0) {
+                    clones[i].transform.position = mainObject.TransformDirection(new Vector2(mainObject.position.x + spacing, mainObject.position.y));
+                }
+            else
+                    clones[i].transform.position = mainObject.TransformDirection(new Vector2(mainObject.position.x  + ((i+1) * spacing), mainObject.position.y));
+         
         }
     }
 
